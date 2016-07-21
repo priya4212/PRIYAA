4a99b51ec1a15b130833e0d351f0805d25484b0a
priya4212 <nspriya805@gmail.com>
2016-07-21 17:09:51 +0530

    Create Maxnumber.java

---
+++ Maxnumber.java
@@ -0,0 +1,23 @@
+package simplecodes;
+
+public class Maxnumber {
+
+	
+	public static void main(String[] args) {
+		int a=24,b=123,c=100;
+		if(a>b && a>c)
+		{
+			System.out.println(a+" is greater");
+		}
+		else if(b>c)
+		{
+		    System.out.println(b+"is greater");
+		}
+		else
+		{
+			System.out.println(c+"is greater");
+		}
+		}
+	
+		
+}
